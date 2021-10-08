07c885c2e3e89ddd996001a5dffbc8146b6c80b3
SterSh <sterlingshaw@gmail.com>
2021-10-08 15:17:18 -0600

    make my line of code

--- src/Schwendinger/Main.java
+++ src/Schwendinger/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println("Hello, my name is Jonathan");
         //Add your print statements here:
         System.out.println("Hello Jonathan, my name is Derrick Eberlein.");
+        //Sterling Code
+        System.out.println("Hello Jonathan, my name is Sterling Shaw.");
 
     }
 }
