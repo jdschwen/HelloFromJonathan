dc8572c7afe5086e6f222ab81683385adcfe4a66
Derrick Eberlein <91962608+ReverendTrivium@users.noreply.github.com>
2021-10-08 10:02:19 -0400

    Added Response Output

--- src/Schwendinger/Main.java
+++ src/Schwendinger/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jonathan");
         //Add your print statements here:
+        System.out.println("Hello Jonathan, my name is Derrick Eberlein.");
 
     }
 }
